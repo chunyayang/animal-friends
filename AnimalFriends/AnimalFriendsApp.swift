2d0d1c229fe8aaa3919a4525b9892ac834954b1b
Chunya Yang <chunya.yg@gmail.com>
2021-07-19 11:49:30 +0800

    Initial Commit

---
+++ AnimalFriends/AnimalFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalFriendsApp.swift
+//  AnimalFriends
+//
+//  Created by Chunya Yang on 2021/7/19.
+//
+
+import SwiftUI
+
+@main
+struct AnimalFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
