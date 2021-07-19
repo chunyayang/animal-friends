10a9e9df576745e7e6a10c30437f8e5399c1c38f
Chunya Yang <chunya.yg@gmail.com>
2021-07-19 13:18:43 +0800

    Project initialization

---
+++ AnimalFriends/Model/Animal.swift
@@ -0,0 +1,8 @@
+//
+//  Animal.swift
+//  AnimalFriends
+//
+//  Created by Chunya Yang on 2021/7/19.
+//
+
+import Foundation
---
+++ AnimalFriends/Model/ModelData.swift
@@ -0,0 +1,8 @@
+//
+//  ModelData.swift
+//  AnimalFriends
+//
+//  Created by Chunya Yang on 2021/7/19.
+//
+
+import Foundation
