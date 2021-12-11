860eb07256cd052ee9dff341da9bcc75e95cc2e7
Chunya Yang <chunya.yg@gmail.com>
2021-12-11 20:36:34 +0800

    Feature: Create a row item component using an animal profile picture as its thumbnail.

---
+++ AnimalFriends/Views/Animals/AnimalRowItem.swift
@@ -0,0 +1,31 @@
+/*
+A single animal row item.
+*/
+
+import SwiftUI
+
+struct AnimalRowItem: View {
+    var animal: Animal
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            animal.picture
+                .resizable()
+                .scaledToFill()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(animal.name)
+                .foregroundColor(.primary)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+struct AnimalRowItem_Previews: PreviewProvider {
+    static let modelData = ModelData()
+
+    static var previews: some View {
+        AnimalRowItem(animal: modelData.animals[0])
+    }
+}
