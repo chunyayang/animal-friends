27418040a7b771af2984b25aae86f226b759c2d2
Chunya Yang <chunya.yg@gmail.com>
2021-12-11 20:50:58 +0800

    Feature: Create a row component that the user can horizontally scroll the animal items and can click one of them to navigate to the animal detail view.

---
+++ AnimalFriends/Views/Animals/AnimalRow.swift
@@ -0,0 +1,32 @@
+/*
+A horizontally scrollable row of animals.
+*/
+
+import SwiftUI
+
+struct AnimalRow: View {
+    var animals: [Animal]
+
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(alignment: .top, spacing: 0) {
+                ForEach(animals) { animal in
+                    NavigationLink(destination: AnimalDetail(animal: animal)) {
+                        AnimalRowItem(animal: animal)
+                    }
+                }
+            }
+        }
+        .frame(height: 185)
+    }
+}
+
+struct AnimalRow_Previews: PreviewProvider {
+    static var animals = ModelData().animals
+
+    static var previews: some View {
+        AnimalRow(
+            animals: Array(animals.prefix(4))
+        )
+    }
+}
