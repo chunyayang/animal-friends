57807e06a7850a35b11702c341b2929f5594084a
Chunya Yang <chunya.yg@gmail.com>
2021-12-16 22:47:53 +0800

    Feature: Add a title above the animal row.

--- AnimalFriends/Views/Animals/AnimalRow.swift
+++ AnimalFriends/Views/Animals/AnimalRow.swift
@@ -5,19 +5,27 @@ A horizontally scrollable row of animals.
 import SwiftUI
 
 struct AnimalRow: View {
+    var rowName: String
     var animals: [Animal]
 
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(alignment: .top, spacing: 0) {
-                ForEach(animals) { animal in
-                    NavigationLink(destination: AnimalDetail(animal: animal)) {
-                        AnimalRowItem(animal: animal)
+        VStack(alignment: .leading) {
+            Text(rowName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(animals) { animal in
+                        NavigationLink(destination: AnimalDetail(animal: animal)) {
+                            AnimalRowItem(animal: animal)
+                        }
                     }
                 }
             }
+            .frame(height: 185)
         }
-        .frame(height: 185)
     }
 }
 
@@ -25,6 +33,6 @@ struct AnimalRow_Previews: PreviewProvider {
     static var animals = ModelData().animals
 
     static var previews: some View {
-        AnimalRow(animals: Array(animals.prefix(4)))
+        AnimalRow(rowName: "Row Name", animals: Array(animals.prefix(4)))
     }
 }
